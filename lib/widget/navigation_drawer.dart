01908a714faf44e44255aaed98cb0b6e13991e1c
Marvin Chin Yi Kai <u2000490@siswa.um.edu.my>
2022-11-08 12:35:48 +0800

    update navigation

--- lib/widget/navigation_drawer.dart
+++ lib/widget/navigation_drawer.dart
@@ -1,41 +1,151 @@
 import 'package:flutter/material.dart';
 
 class NavigationDrawerWidget extends StatelessWidget {
-  final padding = EdgeInsets.symmetric(horizontal: 20);
+  final padding = const EdgeInsets.symmetric(horizontal: 20);
+
+  const NavigationDrawerWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
+    const name = 'Marvin Chin';
+    const id = '#UM00001';
+    const urlImage = 'https://www.worldsbestcatlitter.com/wp-content/uploads/2016/09/cat-affection-blog-image-10.jpg';
+
     return Drawer(
-      child: Container(
+      child: Material(
         //color not decided: pls change RGB value :(
-        color: Color.fromRGBO(50, 75, 205, 1),
+        color: const Color.fromRGBO(50, 75, 205, 1),
         child: ListView(
-          padding: padding,
           children: <Widget>[
-            const SizedBox(height: 48),
-            buildMenuItem(
-              text: 'People',
-              icon: Icons.people,
-            )
+            buildHeader(
+              urlImage: urlImage,
+              name: name,
+              id: id,
+              // disabled no user page yet
+              // onClicked: () => Navigator.of(context).push(MaterialPageRoute(
+              //     builder: (context) => UserPage(//insert page here
+              //       name: name,
+              //       urlImage: urlImage,
+              //     ),)
+            ),
+
+
+            Container(
+              padding: padding,
+              child: Column(
+                children: [
+                  const SizedBox(height: 16),
+                  buildMenuItem(
+                    text: 'People',
+                    icon: Icons.people,
+                    // onClicked: () => selectedItem(context, 0),
+                  ),
+                  const SizedBox(height: 16),
+                  buildMenuItem(
+                    text: 'People',
+                    icon: Icons.people,
+                    // onClicked: () => selectedItem(context, 1),
+                  ),
+                  const SizedBox(height: 16),
+                  buildMenuItem(
+                    text: 'People',
+                    icon: Icons.people,
+                  ),
+                  const SizedBox(height: 16),
+                  buildMenuItem(
+                    text: 'People',
+                    icon: Icons.people,
+                  ),
+
+                  //Divider line
+                  const SizedBox(height: 24),
+                  const Divider(color: Colors.white70),
+                  const SizedBox(height: 24),
+
+                  const SizedBox(height: 16),
+                  buildMenuItem(
+                    text: 'People',
+                    icon: Icons.people,
+                  ),
+                  const SizedBox(height: 16),
+                  buildMenuItem(
+                    text: 'People',
+                    icon: Icons.people,
+                  ),
+                ]
+              )
+
+            ),
           ],
         ),
       ),
     );
   }
 
+  Widget buildHeader({
+    required String urlImage,
+    required String name,
+    required String id,
+    VoidCallback? onClicked,
+  }) =>
+      InkWell(
+        onTap: onClicked,
+        child: Container(
+          padding : padding.add(const EdgeInsets.symmetric(vertical: 40)),
+          child: Row(
+            children: [
+              CircleAvatar(radius: 30, backgroundImage: NetworkImage(urlImage)),
+              SizedBox(width: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: TextStyle(fontSize: 20, color: Colors.white)
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    id,
+                    style: TextStyle(fontSize:14, color: Colors.white)
+                  ),
+                ],
+              ),
+              Spacer(),
+              CircleAvatar(
+                radius: 24,
+                backgroundColor: Color.fromRGBO(30, 60, 168, 1),
+                child: Icon(Icons.add_comment_outlined, color: Colors.white)
+              )
+            ],
+          ),
+        ),
+      );
+
   Widget buildMenuItem({
     required String text,
     required IconData icon,
+    VoidCallback? onClicked,
   }) {
-    final color = Colors.white;
-    final hoverColor = Colors.white70;
+    const color = Colors.white;
+    const hoverColor = Colors.white70;
 
     return ListTile(
       leading: Icon(icon, color: color),
-      title: Text(text, style: TextStyle(color: color)),
+      title: Text(text, style: const TextStyle(color: color)),
       hoverColor: hoverColor,
-      onTap: () {},
+      onTap: onClicked,
     );
   }
-}
 
+  // disabled as no pages created yet
+  // void selectedItem(BuildContext context, int index) {
+  //   switch (index) {
+  //     case 0:
+  //       Navigator.of(context).push(MaterialPageRoute(
+  //         builder: (context) => //insert page here,
+  //       ));
+  //       break;
+  //
+  //   }
+  // }
+}
